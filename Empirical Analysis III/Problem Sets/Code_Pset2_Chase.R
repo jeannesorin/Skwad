2ab8e0c5b111f0a20336d8375c8718f525dfa9d5
Chase Abram <abram@uchicago.edu>
2020-04-15 19:23:34 -0400

    basically done, will clean more

--- Empirical Analysis III/Problem Sets/Code_Pset2_Chase.R
+++ Empirical Analysis III/Problem Sets/Code_Pset2_Chase.R
@@ -44,16 +44,18 @@ nswUT <- subset(nsw, treated == 0)
 # nodegreetest <- olsindeptest(aug(nsw$nodegree), nsw$treated)
 # w76test <- olsindeptest(aug(nsw$w76), nsw$treated)
 
-vars = c("age", "educ", "black", "hisp", "married",
-         "nodegree", "re74")
+vars = c("age", "educ", "black",
+         "hisp", "kidmiss", "kids18",
+         "married", "nodegree", "re74")
 
 for(a in vars) {
   atest <- t.test(nswT[,a], nswUT[,a])
   cat(a, ": ", atest$p.value, "\n")
 }
 
-indep_lm <- glm(treated ~ age + educ + black + hisp + married +
-                nodegree + re74, data = nsw)
+indep_lm <- glm(treated ~ age + educ + black + hisp
+                + hisp + kidmiss + kids18
+                + married + nodegree + metro + re74, data = nsw)
 
 
 # b) If treatment is random, then E[Y(1) - Y(0)] = E[Y(1) | D = 1] - E[Y(0) | D = 0]
@@ -73,8 +75,9 @@ nswcps <- subset(data, (sample == 1 & treated == 1) | sample == 2)
 my_simple_model <- glm(re78 ~ treated, data = nswcps)
 
 # less naive model, includes other demographic factors
-my_standard_model <- glm(re78 ~ treated + age + educ +
-                           black + nodegree + re74, data = nswcps)
+my_standard_model <- glm(re78 ~ treated + age + educ + black
+                         + hisp + kidmiss + kids18
+                         + married + nodegree + metro + re74, data = nswcps)
 
 
 # d)
@@ -135,27 +138,38 @@ drops <- c("dwincl", "early_ra")
 nswcps_m <- nswcps[,!(names(nswcps) %in% drops)]
 nswcps_m <- na.omit(nswcps_m)
 
-nn_match <- matchit(treated ~ age + educ +
-                            black + nodegree + re74,
-                          method = "nearest",
-                          data = nswcps_m)
-summary(nn_model)
+
+nn_match <- matchit(treated ~ age + educ + black
+                    + hisp + kidmiss + kids18
+                    + married + nodegree + metro + re74,
+                    method = "nearest",
+                    data = nswcps_m,
+                    distance = "probit",
+                    discard = "both")
+# summary(nn_model)
 # plot(nn_model)
+plot(nn_match, type = 'jitter', interactive = FALSE)
 
 # get matched dataset
 nswcps_match <- match.data(nn_match)
 
 # check for propensity score support and balancing
-nn_prop_model <- glm(treated ~ age + educ +
-                       black + nodegree + re74, family = binomial(link = "probit"),
+nn_prop_model <- glm(treated ~ age + educ + black 
+                     + hisp + kidmiss + kids18
+                     + married + nodegree + metro + re74, 
+                     family = binomial(link = "probit"),
                      data = nswcps_match)
+
 
 # naive model only looking at treatment
 nn_simple_model <- glm(re78 ~ treated, data = nswcps_match)
 
+
 # less naive model, includes other demographic factors
-nn_standard_model <- glm(re78 ~ treated + age + educ +
-                           black + nodegree + re74, data = nswcps_match)
+nn_standard_model <- glm(re78 ~ treated + age + educ + black 
+                         + hisp + kidmiss + kids18
+                         + married + nodegree + metro + re74, 
+                         data = nswcps_match)
 
 # add propensity scores to nsw + cps data rows
 nn_prop_df <- cbind(prop_score = predict(nn_prop_model,
@@ -167,8 +181,31 @@ nn_prop_df_tr <- subset(nn_prop_df, treated == 1)
 nn_prop_df_untr <- subset(nn_prop_df, treated == 0)
 
 # Occular inspection of propensity score support
-hist(nn_prop_df_tr$prop_score, breaks = 100)
-hist(nn_prop_df_untr$prop_score, breaks = 100)
+hist(nn_prop_df_tr$prop_score, freq = FALSE)
+hist(nn_prop_df_untr$prop_score, add=T)
+
+# ATT
+ATT_match <- mean(nn_prop_df_tr$re78) - mean(nn_prop_df_untr$re78)
+
+# f)
+
+nn_prop_df_tr$re78cf <-rep(0,len = length(nn_prop_df_tr[,1]))
+h <- 0.1
+
+for(i in 1:length(nn_prop_df_tr[,1])) {
+  # construct weights
+  difs <- (nn_prop_df_tr$prop_score[i] - nn_prop_df_untr$prop_score)/h
+  p_weights <- dnorm(difs/h)
+  i_lm <- glm(nn_prop_df_untr$re78 ~ difs, weights = p_weights)
+  nn_prop_df_tr$re78cf[i] <- i_lm$coefficients["(Intercept)"]
+}
+
+ATT_ll <- mean(nn_prop_df_tr$re78) - mean(nn_prop_df_tr$re78cf)
+
+
+
+
+
 
 
 
