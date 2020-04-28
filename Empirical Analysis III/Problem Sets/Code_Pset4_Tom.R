b27c881d899b91766889f74219db7d97f9c2d935
trhierons <44781654+trhierons@users.noreply.github.com>
2020-04-28 18:25:54 -0500

    pretty much done

--- Empirical Analysis III/Problem Sets/Code_Pset4_Tom.R
+++ Empirical Analysis III/Problem Sets/Code_Pset4_Tom.R
@@ -1,12 +1,13 @@
 # Setup: Load packages and prepare data ----------------------------------------
-packages <- c("tidyverse", "foreign", "plm", "rdrobust", 
-              "multiwayvcov","lfe", "stargazar", "rdd")
-set.seed(152427042020)
+packages <- c("tidyverse", "foreign", "plm", "rdrobust", "AER",
+              "multiwayvcov","lfe", "stargazer", "rdd")
+set.seed(15242704)
 lapply(packages, library, character.only = TRUE)
 
 data = as_tibble(read.dta("final5.dta"))%>%
   mutate(schlcode = as.factor(schlcode),
-         p_classize = c_size/(floor((c_size-1)/40)+1) 
+         p_classize = c_size/(floor((c_size-1)/40)+1),
+         enroll_cat = cut(c_size, breaks = seq(from = 0, to = 250, by = 20))
   )
 
 subsample = filter(data, c_size>=20 & c_size<=60) %>%
@@ -135,14 +136,92 @@ iv_nocov <-ivreg(avgmath ~ classize| p_classize, data=data)
 summary(iv_nocov)
 
 # 9. Manipulation --------------------------------------------------------------
-ggplot(data=data, aes(x=weight, color=sex, fill=sex)) + 
-  geom_histogram(aes(y=..density..), alpha=0.5, 
-                 position="identity")+
-  geom_density(alpha=.2) 
-# 10. Miscpecification 1 -------------------------------------------------------
+ggplot(data=data, aes(x=c_size)) + 
+  geom_histogram(aes(y=..density..), alpha=.3, colour = NA, fill = "blue",
+                 position="identity", bins =60)+
+  geom_density()+
+  labs(x="Enrollment", y="Density")+
+  scale_x_continuous(breaks=c(40,80,120,160,200))+
+  scale_y_continuous(breaks=c(0.015))
+  ggsave("figs/manipulation_check.png", width = 7, height = 7)
+  
+# 10.& 11. Miscpecification 1 & 2 ----------------------------------------------
+# Aggregate up
+binned <- data %>% group_by(enroll_cat)%>%
+    summarise_at(c("avgmath", "classize"), mean, na.rm=TRUE)
+binned$midpoint = seq(from = 10, to = 230, by = 20)
 
-# 11. Miscpecification 2 -------------------------------------------------------
+# Plot binned data for maths
+ggplot(data = binned, aes(x = midpoint,y=avgmath))+
+  geom_point()+
+  geom_smooth(method=lm, formula = y~x, se = FALSE)+
+  scale_x_continuous(breaks=seq(from=40,to=200,by=40), minor_breaks = NULL)+
+  scale_y_continuous(breaks=NULL)+
+  ggsave("figs/maths_linear_fit.png", width = 7, height = 7)
+
+ggplot(data = binned, aes(x = midpoint,y=avgmath))+
+  geom_point()+
+  geom_smooth(method=lm, formula = y~x+I(x^2), se = FALSE)+
+  scale_x_continuous(breaks=seq(from=40,to=200,by=40), minor_breaks = NULL)+
+  scale_y_continuous(breaks=NULL)+
+  ggsave("figs/maths_quadratic_fit.png", width = 7, height = 7)
+
+ggplot(data = binned, aes(x = midpoint,y=avgmath))+
+  geom_point()+
+  geom_smooth(method=lm, formula = y~x+I(x^2)+I(x^3) +I(x^4), se = FALSE)+
+  scale_x_continuous(breaks=seq(from=40,to=200,by=40), minor_breaks = NULL)+
+  scale_y_continuous(breaks=NULL)+
+  ggsave("figs/maths_poly_fit.png", width = 7, height = 7)
+
+# And for class size
+ggplot(data = binned, aes(x = midpoint,y=classize))+
+  geom_point()+
+  geom_smooth(method=lm, formula = y~x, se = FALSE)+
+  scale_x_continuous(breaks=seq(from=40,to=200,by=40), minor_breaks = NULL)+
+  scale_y_continuous(breaks=NULL)+
+  ggsave("figs/class_linear_fit.png", width = 7, height = 7)
+
+ggplot(data = binned, aes(x = midpoint,y=classize))+
+  geom_point()+
+  geom_smooth(method=lm, formula = y~x+I(x^2), se = FALSE)+
+  scale_x_continuous(breaks=seq(from=40,to=200,by=40), minor_breaks = NULL)+
+  scale_y_continuous(breaks=NULL)+
+  ggsave("figs/class_quadratic_fit.png", width = 7, height = 7)
+
+ggplot(data = binned, aes(x = midpoint,y=classize))+
+  geom_point()+
+  geom_smooth(method=lm, formula = y~x+I(x^2)+I(x^3) +I(x^4), se = FALSE)+
+  scale_x_continuous(breaks=seq(from=40,to=200,by=40), minor_breaks = NULL)+
+  scale_y_continuous(breaks=NULL)+
+  ggsave("figs/class_poly_fit.png", width = 7, height = 7)
+
 
 # 12. Miscpecification 3 -------------------------------------------------------
+# 1) Bandwidth 
+for (b in seq(from=2,to=20,by=2)){
+  trimmed <- filter(data, 
+                    (abs(c_size-40)<b)|(abs(c_size-80)<b)|
+                      (abs(c_size-120)<b)|(abs(c_size-180)<b)|
+                      (abs(c_size-220)<b))
+  trim_iv <- felm(avgmath ~c_size + tipuach|0|(classize~p_classize)|schlcode,
+                  data = trimmed)
+  print(trim_iv[[7]][4])
+}
+
+
+# 2) Controlling for enrollment with a quadratic term and a polynomial
+iv_quad <- felm(avgmath ~c_size + I(c_size^2) + tipuach|0|(classize~p_classize)|schlcode,
+               data = data)
+summary(iv_quad)
+
+iv_poly <- felm(avgmath ~c_size + I(c_size^2)+I(c_size^3)+I(c_size^4) + tipuach|0|(classize~p_classize)|schlcode,
+                data = data)
+summary(iv_poly)
 
 # 13. Placebo Check ------------------------------------------------------------
+# IV placebo test
+iv_plac <- felm(tipuach ~c_size|0|(classize~p_classize)|schlcode,
+               data = data)
+iv_plac <- ivreg(tipuach ~ classize + c_size|
+        p_classize + c_size, data=data)
+summary(iv_plac)
